146da41a4bff32cd08d4fcf0a3d8cda0c4c4a95a
Ajith Sai Appasu <ajithappasu@gmail.com>
2026-01-13 20:45:09 +0530

    before deleting

--- BACKEND/src/app.js
+++ BACKEND/src/app.js
@@ -4,17 +4,29 @@ const app = express();
 
 // creating server 
 
+// use api will match all the type of users requests get post patch etc ....  
 
+// using +,*, ? operator in  routes 
+
+app.get("/about",(req,res)=>{
+  console.log(req.query);
+   res.send({firstname:"ajith_sai", lastName:"appasu"});
+});
+app.post("/about",(req,res)=>{
+res.send("Data sent successully to the user ");
+});
+app.delete("/about",(req, res)=>{
+    res.send("Data deleted succesfully");
+});
+// app.use("/users",(req,res)=>{
+//     res.end("ajith is the uese..");
+// })
+// // always keep the home route at lsasrt
+// app.use("/",(req,res)=>{
+//     res.end("heloo eatch and every one bbbb ....");
+// })
 app.use("/about",(req,res)=>{
     res.end("this is ajith sai .");
-})
-
-app.use("/users",(req,res)=>{
-    res.end("ajith is the uese..");
-})
-// always keep the home route at lsasrt
-app.use("/",(req,res)=>{
-    res.end("heloo eatch and every one bbbb ....");
 })
 
 app.listen(30000,()=>{
