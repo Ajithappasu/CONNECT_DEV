8fc62fd2ded812a4ea8b498a4f0a6ee90d565c57
Ajith Sai Appasu <ajithappasu@gmail.com>
2026-01-14 11:09:34 +0530

    route handler

--- BACKEND/src/app.js
+++ BACKEND/src/app.js
@@ -1,33 +1,26 @@
 const  express = require('express');
 
 const app = express();
+// route handler 
+// multiple route handlers  , array of route handlers  
+app.use("/users",[(req, res,next)=>{
+ // res.send("hello everyone ");
+  next();
+},
+(req,res, next)=>{
+ //res.send("2 nd response");
+ next();
+},(req, res, next )=>{
+ // res.send(" 3 responce ");
+  next();
+}], (req, res , next)=>{
+ //res.send(" 4 th responce ");
+ next();
+},(req, res, next) =>{
+ res.send("5 th rewsponce ");
+}
+)
 
-// creating server 
-
-// use api will match all the type of users requests get post patch etc ....  
-
-// using +,*, ? operator in  routes 
-
-app.get("/about",(req,res)=>{
-  console.log(req.query);
-   res.send({firstname:"ajith_sai", lastName:"appasu"});
-});
-app.post("/about",(req,res)=>{
-res.send("Data sent successully to the user ");
-});
-app.delete("/about",(req, res)=>{
-    res.send("Data deleted succesfully");
-});
-// app.use("/users",(req,res)=>{
-//     res.end("ajith is the uese..");
-// })
-// // always keep the home route at lsasrt
-// app.use("/",(req,res)=>{
-//     res.end("heloo eatch and every one bbbb ....");
-// })
-app.use("/about",(req,res)=>{
-    res.end("this is ajith sai .");
-})
 
 app.listen(30000,()=>{
 console.log("server successful listining on   ajith 3000.....");
