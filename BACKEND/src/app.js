bff06be11ae5dc1c25d3861189d867b93384bc9b
Ajith Sai Appasu <ajithappasu@gmail.com>
2026-01-16 15:46:37 +0530

    geting dat from the database

--- BACKEND/src/app.js
+++ BACKEND/src/app.js
@@ -4,11 +4,35 @@ const app = express();
 const connectDB= require("./config/DataBase")
 const User = require("./scema/user");
 
-app.use(express.json());
 
+
+// to display the data of the users
+app.get("/getdata", async (req,res)=>{
+    // this User is the one we iported from above 
+  const allUsers = await User.find({});
+  res.send(allUsers);
+
+});
+
+// display dat of single user by emaild 
+app.get("/getone", async (req, res)=>{
+    const oneUser = await User.findOne({emailId: "appasuajithsai111@gmail.com"})
+    res.send(oneUser);
+})
+
+
+
+
+
+
+// we use this middleWare to  convert  json format request data to  java script object format. 
+app.use(express.json());
+// we use use middleware because it can be used in all routes and all typres (get, post etc).
 
 app.post("/signup", async(req,res)=>{
+    // we are getting data in   raw formmt in the request body  
 const user = new User(req.body);
+// geting data from body ;
 try{
 await user.save();
 res.send("data saved successfully");
