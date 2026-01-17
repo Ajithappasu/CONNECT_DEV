f804ab9a9bc291c7c9a6acc097c468797083f4b7
Ajith Sai Appasu <ajithappasu@gmail.com>
2026-01-17 20:37:35 +0530

    restricting the update features

--- BACKEND/src/app.js
+++ BACKEND/src/app.js
@@ -1,7 +1,7 @@
-const  express = require('express');
+const express = require('express');
 
 const app = express();
-const connectDB= require("./config/DataBase")
+const connectDB = require("./config/DataBase")
 const User = require("./scema/user");
 
 // we use this middleWare to  convert  json format request data to  java script object format. 
@@ -12,46 +12,54 @@ app.use(express.json());
 
 
 // to display the data of the users
-app.get("/getdata", async (req,res)=>{
- try{   // this User is the one we iported from above 
-  const allUsers = await User.find({});
-  res.send(allUsers);
- }catch(err){
-    res.send("someting went woring  error occured "+err);
- }
+app.get("/getdata", async (req, res) => {
+    try {   // this User is the one we iported from above 
+        const allUsers = await User.find({});
+        res.send(allUsers);
+    } catch (err) {
+        res.send("someting went woring  error occured " + err);
+    }
 });
 
 // display dat of single user by emaild 
-app.get("/getone", async (req, res)=>{
-    try{
-    const oneUser = await User.findOne({emailId: "appasuajithsai111@gmail.com"})
-    res.send(oneUser);
-    }catch(err){
-    res.send("someting went woring  error occured "+err);
- }
+app.get("/getone", async (req, res) => {
+    try {
+        const oneUser = await User.findOne({ emailId: "appasuajithsai111@gmail.com" })
+        res.send(oneUser);
+    } catch (err) {
+        res.send("someting went woring  error occured " + err);
+    }
 })
 /// to delete use by id 
-app.delete("/delete",  async (req, res)=>{
+app.delete("/delete", async (req, res) => {
     const id = req.body.id;
     console.log(id);
-    try{
-    await User.findByIdAndDelete({_id:id });
-    res.send("deleted successfully ");
-    }catch(err){
-    res.send("someting went woring  error occured "+err);
- }
+    try {
+        await User.findByIdAndDelete({ _id: id });
+        res.send("deleted successfully ");
+    } catch (err) {
+        res.send("someting went woring  error occured " + err);
+    }
 })
 
 // to update user by id 
-app.patch("/update",async (req, res)=>{
-    const  ID = req.body.Id;
+app.patch("/update/:Id", async (req, res) => {
+   
+    try {
+         const ID = req.params?.Id;
     const data = req.body;
-    try{
-    await User.findByIdAndUpdate({_id:ID}, data );
-    res.send("updated succesfully");
-    }catch(err){
-    res.send("someting went woring  error occured "+err);
- }
+    const AllowedUpdates =["firstName", "lastName", "password","age", "about","skills"]
+    const isUpdatesAllowed = Object.keys(data).every((k)=>{
+        return AllowedUpdates.includes(k);
+    });
+    if(!isUpdatesAllowed){
+        throw new Error("update not allowed");
+    }
+        await User.findByIdAndUpdate({ _id: ID }, data);
+        res.send("updated succesfully");
+    } catch (err) {
+        res.send("someting went woring  error occured " + err);
+    }
 })
 
 
@@ -59,26 +67,26 @@ app.patch("/update",async (req, res)=>{
 
 
 
-app.post("/signup", async(req,res)=>{
+app.post("/signup", async (req, res) => {
     // we are getting data in   raw formmt in the request body  
-const user = new User(req.body);
-// geting data from body ;
-try{
-await user.save();
-res.send("data saved successfully");
-}catch(err){
-    res.send("error oucered "+err);
-}
+    const user = new User(req.body);
+    // geting data from body ;
+    try {
+        await user.save();
+        res.send("data saved successfully");
+    } catch (err) {
+        res.send("error oucered " + err);
+    }
 })
 
 connectDB()
-.then(()=>{
-    console.log("connection established ");
-// ew have to run the server only when the database is connected successfully .
-    app.listen(30000,()=>{
-console.log("server successful listining on   ajith 3000.....");
-});
-})
-.catch(()=>{
-    console.log("error not connected ");
-})
+    .then(() => {
+        console.log("connection established ");
+        // ew have to run the server only when the database is connected successfully .
+        app.listen(30000, () => {
+            console.log("server successful listining on   ajith 3000.....");
+        });
+    })
+    .catch(() => {
+        console.log("error not connected ");
+    })
