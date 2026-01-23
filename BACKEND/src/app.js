48856548df07781525165e0bd093f6cc6efdcebc
Ajith Sai Appasu <ajithappasu@gmail.com>
2026-01-23 14:19:33 +0530

    added middle ware Userauth

--- BACKEND/src/app.js
+++ BACKEND/src/app.js
@@ -7,6 +7,7 @@ const {validateSignUpData}  = require("./utils/validation");
 const bcrypt= require("bcrypt");
 const cookieParser = require("cookie-parser");
 const jwt = require("jsonwebtoken");
+const {userAuth} = require("./middlewares/auth")
 
 
 
@@ -17,62 +18,6 @@ app.use(express.json());
 // we use this middle ware so tha we can read cookie 
 app.use(cookieParser());
 
-
-// to display the data of the users
-app.get("/getdata", async (req, res) => {
-
-    try {   // this User is the one we iported from above 
-        const allUsers = await User.find({});
-        res.send(allUsers);
-    } catch (err) {
-      return   res.send("someting went woring  error occured " + err);
-    }
-});
-
-// display dat of single user by emaild 
-app.get("/getone", async (req, res) => {
-    try {
-        const oneUser = await User.findOne({ emailId: "appasuajithsai111@gmail.com" })
-        res.send(oneUser);
-    } catch (err) {
-        res.send("someting went woring  error occured " + err);
-    }
-})
-/// to delete use by id 
-app.delete("/delete", async (req, res) => {
-    const id = req.body.id;
-    console.log(id);
-    try {
-        await User.findByIdAndDelete({ _id: id });
-        res.send("deleted successfully ");
-    } catch (err) {
-        res.send("someting went woring  error occured " + err);
-    }
-})
-
-// to update user by id 
-app.patch("/update/:Id", async (req, res) => {
-   
-    try {
-         const ID = req.params?.Id;
-    const data = req.body;
-    // we declare the  parametes that are  allowd for updating
-    const AllowedUpdates =["firstName", "lastName", "password","age", "about","skills"]
-    const isUpdatesAllowed = Object.keys(data).every((k)=>{
-        return AllowedUpdates.includes(k);
-    });
-    if(!isUpdatesAllowed){
-        throw new Error("update not allowed");
-    }
-    if(data?.skills.length>10){
-        throw new Error("skills must not be grater than 10");
-    }
-        await User.findByIdAndUpdate({ _id: ID }, data);
-        res.send("updated succesfully");
-    } catch (err) {
-        res.send("someting went woring  error occured " + err);
-    }
-})
 
 app.post("/signup", async (req, res) => {
     // we are getting data in   raw formmt in the request body  
@@ -93,25 +38,6 @@ validateSignUpData(req)
     }
 })
 
-
-app.get("/profile",async(req,res)=>{
-    try{
-    const cookies = req.cookies;
-    const {token}=cookies;
-    if(!token){
-        throw new Error("invalid token");
-    }
-    const decodedMessage = await jwt.verify(token,"Dev_connect");
-    const{_id}= decodedMessage;
- const user =  await User.findById(_id);
- if(!user){
-    throw new Error("invalid user");
- }
-    res.send(user);
-    }catch(err){
-        res.status(404).send("error occured"+err);
-    }
-})
 // login api  user logined in 
 app.post("/login", async(req,res)=>{
     try{
@@ -140,6 +66,23 @@ res.send("login successful");
       res.status(404).send("error oucered " + err);
     }
 })
+// to see profile of the user 
+app.get("/profile",userAuth,async(req,res)=>{
+    try{
+        const user = req.user;
+    res.send(user);
+    }catch(err){
+        res.status(404).send("error occured"+err);
+    }
+})
+
+app.post("/sendConnectionRequest",userAuth,async(req, res)=>{
+// sending connection request
+const user = req.user;
+
+res.send("sending connection request"+user.firstName);
+})
+
 
 connectDB()
     .then(() => {
---
+++ BACKEND/src/middlewares/auth.js
@@ -0,0 +1,30 @@
+const jwt = require("jsonwebtoken");
+const User = require("../scema/user");
+const userAuth = async (req, res, next)=>{
+    try{
+ const{token}= req.cookies;
+ if(!token){
+    throw new Error("invalid token");
+ }
+ const decodedMessage = await jwt.verify(token,"Dev_connect");
+  const{_id}= decodedMessage;
+   
+  const user = await User.findById(_id);
+  if(!user){
+    throw new Error("user not exits ");
+  }
+// we are  giving the value of the user to the request user 
+  req.user=user
+ next();
+
+
+
+    }catch(err){
+        res.status(404).send("error occured"+err);
+    }
+
+}
+
+module.exports={
+    userAuth,
+};
